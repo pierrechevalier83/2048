5f50285b73e08ba00c6ec816e169b9487a47224f
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-01-05 23:28:12 +0000

    Implement human moves

--- 2048.cxx
+++ 2048.cxx
@@ -15,18 +15,18 @@ using Matrix = vector<Row>;
 class Board {
    public:
     Board() {
-	    // initialize ncurses
-	    initscr();
-		// don't buffer characters
-		cbreak();
-		// don't print input
+        // initialize ncurses
+        initscr();
+        // don't buffer characters
+        cbreak();
+        // don't print input
         noecho();
-		// capture arrow keys
-		keypad(stdscr, true);
-	}
+        // capture arrow keys
+        keypad(stdscr, true);
+    }
     void print(const Matrix& data) {
-	    refresh();
-		move(0,0);
+        refresh();
+        move(0, 0);
         size_t n_rows = 0;
         for (const auto& row : data) {
             n_rows = row.size();
@@ -43,7 +43,7 @@ class Board {
         addch('+');
         for (size_t i = 0; i < n; ++i) {
             print_n_chars(cell_width, '-');
-			addch('+');
+            addch('+');
         }
         addch('\n');
     }
@@ -64,7 +64,7 @@ class Board {
         new_line();
     }
     void print_value(boost::optional<int> value) {
-		if (value) {
+        if (value) {
             if (*value == 0) {
                 print_centered(".", cell_width);
             } else {
@@ -80,15 +80,15 @@ class Board {
         } else {
             auto padding = (width - content.length()) / 2;
             print_n_chars(padding, ' ');
-            //cout << setw(width - padding) << left <<
-			printw("%-*s", width - padding, content.c_str()); // TODO: format
+            // cout << setw(width - padding) << left <<
+            printw("%-*s", width - padding, content.c_str());  // TODO: format
         }
     }
-	void print_n_chars(size_t n, char c) {
-	    for (size_t i = 0; i < n; ++i) {
-		    addch(c);
-		}
-	}
+    void print_n_chars(size_t n, char c) {
+        for (size_t i = 0; i < n; ++i) {
+            addch(c);
+        }
+    }
     void print_sep() { addch('|'); }
     void new_line() { addch('\n'); }
     const int cell_width = 7;
@@ -98,9 +98,7 @@ enum class Status { ongoing, interrupted, lost, won };
 
 class Game {
    public:
-    Game() {
-        srand(time(0));
-	}
+    Game() { srand(time(0)); }
     unique_ptr<Matrix> initialize(int n_rows, int n_cols) {
         auto data = make_unique<Matrix>(Matrix(n_rows, Row(n_cols, 0)));
         randomly_insert(2, *data);
@@ -109,55 +107,108 @@ class Game {
     }
     Status play(Matrix& data) {
         auto status = human_play(data);
-		if (status == Status::ongoing) { 
+        if (status == Status::ongoing) {
             status = computer_play(data);
-		}
-		return status;
+        }
+        return status;
     }
 
    private:
     bool randomly_insert(int value, Matrix& data) {
-		vector<pair<size_t, size_t>> zeroes;
-		for (size_t ii = 0; ii < data.size(); ++ii) {
-		    for (size_t jj = 0; jj < data[ii].size(); ++ jj) {
-			    if (data[ii][jj] == 0) {
-				    zeroes.push_back(make_pair(ii, jj));
-				}
-			}
-		}
-		if (zeroes.empty()) {
-		    return false;
-		}
+        vector<pair<size_t, size_t>> zeroes;
+        for (size_t ii = 0; ii < data.size(); ++ii) {
+            for (size_t jj = 0; jj < data[ii].size(); ++jj) {
+                if (data[ii][jj] == 0) {
+                    zeroes.push_back(make_pair(ii, jj));
+                }
+            }
+        }
+        if (zeroes.empty()) {
+            return false;
+        }
         auto& pos = zeroes[rand() % zeroes.size()];
         data[pos.first][pos.second] = value;
-		return true;
+        return true;
     }
-    Status human_play(const Matrix&) {
-	    //cout << flush;
-		//char input;
-		int input = getch();
-		//cin >> input;
-		if (input == 'q') {
-		    printw("See you later!\n");
-			return Status::interrupted;
-		}
-		cin.clear();
-        // TODO: take input from cin and update data appropriately
-		return Status::ongoing;
+    Status human_play(Matrix& data) {
+        int input = getch();
+        if (input == 'q') {
+            printw("See you later!\n");
+            return Status::interrupted;
+        } else if (input == KEY_UP) {
+            data = up(data);
+        } else if (input == KEY_DOWN) {
+            data = down(data);
+        } else if (input == KEY_RIGHT) {
+            data = right(data);
+        } else if (input == KEY_LEFT) {
+            data = left(data);
+        }
+        return Status::ongoing;
+    }
+    Matrix up(const Matrix& data) {
+        return rotate_left(left(rotate_right(data)));
+    }
+    Matrix down(const Matrix& data) {
+        return rotate_left(right(rotate_right(data)));
+    }
+    Matrix rotate_left(const Matrix& data) {
+        assert(!data.empty());
+        Matrix rotated(data[0].size(), Row(data.size()));
+        for (size_t i = 0; i < data.size(); ++i) {
+            for (size_t j = 0; j < data[i].size(); ++j) {
+                rotated[j][i] = data[i][j];
+            }
+        }
+        return rotated;
+    }
+    Matrix rotate_right(const Matrix& data) {
+        return rotate_left(rotate_left(rotate_left(data)));
+    }
+    Matrix right(const Matrix& data) {
+        Matrix copy = data;
+        for (auto& row : copy) {
+            row = slide(row);
+        }
+        return copy;
+    }
+    Matrix left(const Matrix& data) {
+        Matrix copy = data;
+        for (auto& row : copy) {
+            auto reversed = row;
+            reverse(reversed.begin(), reversed.end());
+            row = slide(reversed);
+            reverse(row.begin(), row.end());
+        }
+        return copy;
+    }
+    Row slide(const Row& r) {
+        auto row = r;
+        stable_partition(row.begin(), row.end(),
+                         [](auto val) { return val == 0; });
+        int previous_value = 0;
+        for (auto it = row.end(); it != row.begin(); --it) {
+            previous_value = *it;
+            if (*(it - 1) == previous_value) {
+                *it *= 2;
+                *(it - 1) = 0;
+                rotate(row.begin(), it - 1, it);
+            }
+        }
+        return row;
     }
     Status computer_play(Matrix& data) {
-	    int value = 2;
-		if (rand() % 10 == 1) {
-		    value = 4;
-		}
-		if (!randomly_insert(value, data)) {
-		    printw("Game over!\n");
-			return Status::interrupted;
-		}
-		return Status::ongoing;
+        int value = 2;
+        if (rand() % 10 == 1) {
+            value = 4;
+        }
+        if (!randomly_insert(value, data)) {
+            printw("Game over!\n");
+            return Status::interrupted;
+        }
+        return Status::ongoing;
     }
 };
-
 
 }  // namespace game_on
 
@@ -166,9 +217,9 @@ int main() {
     game_2048::Board board;
     auto data = *game.initialize(4, 4);
     auto status = game_2048::Status::ongoing;
-	while (status == game_2048::Status::ongoing) {
-	    board.print(data);
-		status = game.play(data);
+    while (status == game_2048::Status::ongoing) {
+        board.print(data);
+        status = game.play(data);
     }
     return 0;
 }
